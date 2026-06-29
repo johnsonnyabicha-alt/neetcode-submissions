bc5430bf5a1c85a18cb1b510fcc22b4861623a17
johnson1 <jon202@exeter.ac.uk>
2026-06-29 23:29:55 +0100

    Add: is-anagram - submission-14

---
+++ Data Structures & Algorithms/is-anagram/submission-14.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # if len(s) != len(t):
+        #     return False
+        # s_list = list(t)
+        # counter = 0
+        # len1, len2 = len(s), len(t)
+        # for char in s:
+        #     if char in s_list:
+        #         s_list.remove(char)
+        #         counter += 1
+
+        # if counter == len1:
+        #     return True 
+        # else:
+        #     return False
+
+
+        
+        s_list= defaultdict(int)
+        b_list = defaultdict(int)
+        for a in s:
+            s_list[a] += 1
+        for b in t:
+            b_list[b] += 1
+        if sorted(s_list.items()) == sorted(b_list.items()):
+            return True 
+        else:
+            return False
+        
+
+        
